eb2b600534d9af9987deedb25508abb601ae529f
techkapasi <techkapasi@gmail.com>
2018-04-25 08:32:44 +0500

    blogs data in JSON

--- src/Blogs.js
+++ src/Blogs.js
@@ -5,16 +5,17 @@ export default class Home extends Component {
 
     getBlogs() {
         let url, promise;
-        url = "https://cors-anywhere.herokuapp.com/https://medium.com/feed/@freecodecamp";
+        url = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@freecodecamp";
+        //url = "https://medium.com/feed/@freecodecamp";
 
         axios.get(url)
-            .then(res => console.log(res))
+            .then(res => console.log(res.data.items))
             .catch(err => console.log(err));
 
     }
 
     render() {
-        //this.getBlogs();
+        this.getBlogs();
         return (
             <div>
 
