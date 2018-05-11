6afddf757f93fb25855d300000c1f67e5ef71a45
Abdul Moiz Ansari <a.moiz471@gmail.com>
2018-05-12 00:28:26 +0500

    push-part1

---
+++ part 1/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import 'bulma/css/bulma.css';
+
+import Home from './Home';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+      
+        <Home />
+
+      </div>
+    );
+  }
+}
+
+export default App;
